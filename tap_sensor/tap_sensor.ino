4566ced4f136ebd87a2f855148c9f129ded331be
CTbossclb <110459397+CTbossclb@users.noreply.github.com>
2023-05-09 19:58:08 +0200

    Add files via upload

---
+++ tap_sensor/tap_sensor.ino
@@ -0,0 +1,31 @@
+#define led 9
+#define pin 8
+
+boolean trig = true;
+boolean state = true;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(pin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  trig = digitalRead(pin);
+  if(!trig && state == false)
+  {
+    digitalWrite(led, HIGH);
+    Serial.println("ON");
+    state = true;
+    delay(500);  
+  }
+  else if(!trig && state == true)
+  {
+    digitalWrite(led, LOW);
+    Serial.println("OFF");
+    state = false;
+    delay(500); 
+  }  
+}
